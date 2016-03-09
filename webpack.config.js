fe45b8e864d4be9c91dd877c0ceca630097ad205
Raul Matei <raulmatei@gmail.com>
2016-03-09 19:14:16 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var path = require('path');
+var webpack = require('webpack');
 
 module.exports = {
   entry: {
@@ -21,5 +22,13 @@ module.exports = {
 
   resolve: {
     extensions: ['', '.js', '.jsx']
-  }
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development')
+      }
+    })
+  ]
 };
